33fd547d93f738135cb966a3430a20a4d542cbd2
vimal Raveendran <vimalraveendra@gmail.com>
2020-09-16 13:08:52 +0200

    Created Pagination component & also updated the App component

--- Components/Carousel/Carousel.js
+++ Components/Carousel/Carousel.js
@@ -1,36 +1,38 @@
 import React from 'react';
 
-import {Image, StyleSheet, ScrollView, Dimensions} from 'react-native';
+import {Image, StyleSheet, ScrollView, Dimensions, View} from 'react-native';
 
 const {width} = Dimensions.get('window');
 const height = (width * 100) / 60; // 80%
 
 const Carousel = ({images, setChange}) => {
   return (
-    <ScrollView
-      horizontal
-      pagingEnabled
-      scrollEventThrottle
-      onScroll={setChange}
-      showsHorizontalScrollIndicator={false}>
-      {images.map((image, index) => (
-        <Image
-          key={index}
-          style={styles.image}
-          source={{
-            uri: image,
-          }}
-        />
-      ))}
-    </ScrollView>
+    <View style={styles.imageScroll}>
+      <ScrollView
+        horizontal
+        pagingEnabled
+        scrollEventThrottle
+        onScroll={setChange}
+        showsHorizontalScrollIndicator={false}>
+        {images.map((image, index) => (
+          <Image
+            key={index}
+            style={styles.image}
+            source={{
+              uri: image,
+            }}
+          />
+        ))}
+      </ScrollView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   image: {
     width: width,
-    height: height,
-    resizeMode: 'contain',
+    height: height - 100,
+    resizeMode: 'cover',
   },
 });
 
